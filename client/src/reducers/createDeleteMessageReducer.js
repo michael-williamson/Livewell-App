af26e9b0464017e9aa0f055fd867edbc6ca914eb
michael-williamson <michaeldavid1721@yahoo.com>
2020-07-17 17:11:33 -0500

    changed up the reducer

--- client/src/reducers/createDeleteMessageReducer.js
+++ client/src/reducers/createDeleteMessageReducer.js
@@ -3,9 +3,9 @@ import {CREATE_FISH, DELETE_FISH} from '../actions/types';
 export default (state = {}, action) => {
     switch(action.type){
         case CREATE_FISH:
-            return [action.payload];
+            return {...action.payload};
         case DELETE_FISH:
-            return [action.payload]
+            return {...action.payload}
         default:
             return state;
     }
