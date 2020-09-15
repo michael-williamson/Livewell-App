f645c72484b33ad4e49da83111ee7ac14f4471b4
michael-williamson <michaeldavid1721@yahoo.com>
2020-09-14 22:08:24 -0500

    minor formatting

--- client/src/components/FishDelete.js
+++ client/src/components/FishDelete.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import Modal from './Modal';
-import {connect} from 'react-redux';
-import {deleteFish} from '../actions/index';
+import React, { Component } from "react";
+import Modal from "./Modal";
+import { connect } from "react-redux";
+import { deleteFish } from "../actions/index";
 
-import requireAuth from './requireAuth';
- 
+import requireAuth from "./requireAuth";
+
 class FishDelete extends Component {
-
-
-    render() {
-        return (
-            <div>
-              <Modal deleteFish={this.props.deleteFish} id={this.props.match.params.id} googleId={this.props.auth?.userId}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Modal
+          deleteFish={this.props.deleteFish}
+          id={this.props.match.params.id}
+          googleId={this.props.auth?.userId}
+        />
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {  
-    auth: state.Auth
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    auth: state.Auth,
+  };
+};
 
-export default connect(mapStateToProps,{deleteFish})(requireAuth(FishDelete));
+export default connect(mapStateToProps, { deleteFish })(
+  requireAuth(FishDelete)
+);
--- client/src/components/FishEnter.js
+++ client/src/components/FishEnter.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import FishEnterForm from './FishEnterForm';
-import '../styles/FishEnter.css';
+import FishEnterForm from "./FishEnterForm";
+import "../styles/FishEnter.css";
 
-import requireAuth from './requireAuth';
+import requireAuth from "./requireAuth";
 
 class FishEnter extends Component {
-    render() {
-        return (
-            <div className="main">
-                <FishEnterForm/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="main">
+        <FishEnterForm />
+      </div>
+    );
+  }
 }
 
 export default requireAuth(FishEnter);
