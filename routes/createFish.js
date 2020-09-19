7a212642160ab94b652384585b92cd62f58b5379
michael-williamson <michaeldavid1721@yahoo.com>
2020-09-18 23:41:19 -0500

    minor formatting

--- routes/createFish.js
+++ routes/createFish.js
@@ -1,52 +1,33 @@
-const mongoose = require('mongoose');
-const User = mongoose.model('user');
-const Fish = mongoose.model('fish');
+const mongoose = require("mongoose");
+const User = mongoose.model("user");
+const Fish = mongoose.model("fish");
 
+module.exports = (app) => {
+  app.post("/api/createFish", (req, res) => {
+    const { id, species, inches, pounds, ounces } = req.body;
 
-module.exports = app => {
-    app.post('/api/createFish', (req, res) => {
-        const {
-            id,
-            species,
-            inches,
-            pounds,
-            ounces
-        } = req.body;
+    const fish = new Fish({
+      species,
+      inches,
+      pounds,
+      ounces,
+    });
 
-        const fish = new Fish({
-            species,
-            inches,
-            pounds,
-            ounces
-        });
-
-        User
-        .findOne({googleId: id})
-        .then((user) => {
-            if (!user) {
-                let newUser = new User({googleId: id});
-                newUser
-                    .fish
-                    .push(fish);
-                Promise.all([
-                    newUser.save(),
-                    fish.save()
-                ]).then(() =>  res.send({}))
-                .catch(err=>
-                    console.log(err));
-
-            } else {
-                user
-                    .fish
-                    .push(fish);
-                Promise.all([
-                    user.save(),
-                    fish.save()
-                ]).then(() => res.send({message:"Creation Successful"}))
-                .catch(err => console.log(err))
-            }
-        })
-        .catch(err => console.log(err));
-
-    })
-}
+    User.findOne({ googleId: id })
+      .then((user) => {
+        if (!user) {
+          let newUser = new User({ googleId: id });
+          newUser.fish.push(fish);
+          Promise.all([newUser.save(), fish.save()])
+            .then(() => res.send({}))
+            .catch((err) => console.log(err));
+        } else {
+          user.fish.push(fish);
+          Promise.all([user.save(), fish.save()])
+            .then(() => res.send({ message: "Creation Successful" }))
+            .catch((err) => console.log(err));
+        }
+      })
+      .catch((err) => console.log(err));
+  });
+};
