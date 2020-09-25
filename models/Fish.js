281b3da5ad6b493465f51ad254c99236ce42a6a0
michael-williamson <michaeldavid1721@yahoo.com>
2020-09-24 21:31:07 -0500

    minor formatting

--- models/Fish.js
+++ models/Fish.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const fishSchema = new Schema({
-    species: String,
-    inches: String,
-    pounds: String,
-    ounces: String
+  species: String,
+  inches: String,
+  pounds: String,
+  ounces: String,
 });
 
-mongoose.model('fish',fishSchema);
+mongoose.model("fish", fishSchema);
